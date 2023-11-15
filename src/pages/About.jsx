b14ab05f508b7d62a485fc8d7b81690898427fdf
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-11-15 18:24:34 +0100

    delete typed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./css/About.scss";
-import Typed from "react-typed";
 
 const About = () => {
   const [windowSize, setWindowSize] = useState(window.innerWidth);
@@ -62,24 +61,6 @@ const About = () => {
           </div>
         </section>
       </section>
-      <div className="about-mobile-container">
-        <Typed
-          className="typed-text-about"
-          strings={[
-            `Je m'appelle Guillaume Leroy. Développeur Web Junior à Lille, j'ai 28 ans. Suite à une
-            reconversion de carrière, j'ai décidé de me lancer dans le
-            développement web. J'ai suivi une formation et des cours en ligne pour maîtriser les
-            langages de programmation, notamment HTML, CSS, JavaScript, ainsi
-            que des frameworks populaires tels que React et NodeJS. <br />
-            Mon ambition est de m'investir dans des projets stimulants,
-            d'acquérir une expérience pratique significative, et de participer
-            à la création de solutions créatives et innovantes. Je suis
-            actuellement ouvert aux opportunités de stage ou d'emploi dans le
-            domaine du développement web.`,
-          ]}
-          typeSpeed={1}
-        />
-      </div>
     </main>
   );
 };
