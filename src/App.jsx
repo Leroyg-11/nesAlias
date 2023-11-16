32a8640a1f4ff20213b8a127833e25af33acab61
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-11-16 13:46:30 +0100

    match test

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ import Contact from "./pages/Contact";
 import Error from "./pages/Error";
 
 const App = () => {
+  const ErrorCustom = () => {
+    return (
+      <div>
+        <h1>Erreur personnalisée</h1>
+        <p>L'URL que vous avez saisie est incorrecte.</p>
+      </div>
+    );
+  };
   return (
     <BrowserRouter>
       <Routes>
@@ -16,6 +24,12 @@ const App = () => {
           <Route path="about" element={<About />} />
           <Route path="projet" element={<Projet />} />
           <Route path="contact" element={<Contact />} />
+          <Route
+            path="error"
+            element={(match) => {
+              return <Redirect to="/error-custom" />;
+            }}
+          />
         </Route>
         <Route path="*" element={<Error />} />
       </Routes>
